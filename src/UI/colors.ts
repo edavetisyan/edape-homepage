11f093672e37def55009330adb71963efcd0bbd8
edavetisyan <edmondavetisyanw@gmail.com>
2022-07-02 19:12:10 +0300

    feat: add UI colors

---
+++ src/UI/colors.ts
@@ -0,0 +1,56 @@
+interface iColors {
+  polarNight: {
+    nord0: string,
+    nord1: string,
+    nord2: string,
+    nord3: string,
+
+  },
+  snowStorm: {
+    nord4: string,
+    nord5: string,
+    nord6: string,
+  },
+  frost: {
+    nord7: string,
+    nord8: string,
+    nord9: string,
+    nord10: string,
+  },
+  aurora: {
+    nord11: string,
+    nord12: string,
+    nord13: string,
+    nord14: string,
+    nord15: string,
+  }
+}
+
+const colors: iColors = {
+  polarNight: {
+    nord0: "#2E3440",
+    nord1: "#3B4252",
+    nord2: "#434C5E",
+    nord3: "#4C566A"
+  },
+  snowStorm: {
+    nord4: "#D8DEE9",
+    nord5: "#E5E9F0",
+    nord6: "#ECEFF4"
+  },
+  frost: {
+    nord7: "#8FBCBB",
+    nord8: "#88C0D0",
+    nord9: "#81A1C1",
+    nord10: "#5E81AC"
+  },
+  aurora: {
+    nord11: "#BF616A",
+    nord12: "#D08770",
+    nord13: "#EBCB8B",
+    nord14: "#A3BE8C",
+    nord15: "#B48EAD"
+  }
+}
+
+export default colors
